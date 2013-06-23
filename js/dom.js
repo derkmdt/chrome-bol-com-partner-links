f94d0b1879fce2d8b19f15f69e7da1fd3f4ee4d0
Derk Braakman <derkmdt@hotmail.com>
2013-06-23 22:55:47 +0200

    Do not show on bol.com subdomains

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.url == 'getPRODUCT') {
-		if(location.href.indexOf("bol.com") != -1) { // && location.href.indexOf("\/p\/") != "-1"
+		var isSubdomain = function(url) {
+			var regex = new RegExp(/^([a-z]+\:\/{2})?([\w-]+\.[\w-]+\.\w+)$/);
+			return url.match(regex) ? true : false;
+		}
+		if(location.href.indexOf("bol.com") != -1 && !isSubdomain(location.href)) { // && location.href.indexOf("\/p\/") != "-1"
 			var product = new Object();
 			product.url = location.href.split("?")[0];
 			product.title = document.title;
