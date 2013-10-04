687a3bb1f74d8bfd74c12a6b749b87c797c66cd2
Derk Braakman <derkmdt@hotmail.com>
2013-10-04 14:59:47 +0200

    added changes

--- js/background.js
+++ js/background.js
@@ -1,7 +1,8 @@
 chrome.tabs.onUpdated.addListener(function( tabId , info, tab ) {
     if ( info.status == "complete" ) {
        	getProductData(tab, function(data) {
-			if (!data.product) {
+	       	console.log('data.product',data.product.url);
+			if (data.product.url == '') {
 				chrome.pageAction.hide(tabId);
 			} else {
 				chrome.pageAction.show(tabId);
@@ -11,10 +12,17 @@ chrome.tabs.onUpdated.addListener(function( tabId , info, tab ) {
 });
 var getProductData = function(tab, callback) {
 	if(!tab) return;
+	var isSubdomain = function(url) {
+		var regex = new RegExp(/^([a-z]+\:\/{2})?([\w-]+\.[\w-]+\.\w+)$/);
+		return url.match(regex) ? true : false;
+	}
 	var protocol =  tab.url.match(/^(.[^:]+):/)[1];
-	if(protocol == "https" || protocol == "http") {
-		chrome.tabs.sendMessage(tab.id, {url: "getPRODUCT",taburl: tab.url}, function(response) {
-			callback(response);
+	if(protocol == "https" || protocol == "http" || (tab.url.indexOf("bol.com") != -1 && !isSubdomain(tab.url)) ) {
+		chrome.tabs.sendMessage(tab.id, {url: "getPRODUCT", taburl: tab.url}, function(response) {
+			console.log('getPRODUCT',response);
+			if(response) callback(response); else callback();
 		});
-	};
+	} else {
+		return;
+	}
 };
